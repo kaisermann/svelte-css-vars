f40ffee3ac4e5bea4c4ae0c540e873690ceba4bf
Ben Parsons <9ParsonsB@users.noreply.github.com>
2021-06-17 13:09:20 +1000

    fix: use ElementCSSInlineStyle in type definitions
    ElementCSSInline (interface) should match to any element (not just HTMLElements) that can contain an inline style

--- index.d.ts
+++ index.d.ts
@@ -1,7 +1,7 @@
 declare module 'svelte-css-vars' {
   export function update(new_props: Record<string, any>);
 
-  function apply(node: HTMLElement, props: Record<string, any>);
+  function apply(node: ElementCSSInlineStyle, props: Record<string, any>);
 
   export default apply;
 }
